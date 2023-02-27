92b6bebd41c89a450284db73d29426a0e8dabdc5
Vojtech Suchanek <suchavo3@cvut.cz>
2023-02-27 19:59:50 +0100

    Eshop

--- slices/History/index.js
+++ slices/History/index.js
@@ -1,36 +1,27 @@
-import React from 'react'
-import { PrismicRichText } from '@prismicio/react'
+import React from "react";
+import { PrismicRichText } from "@prismicio/react";
+import styled from "styled-components";
+import { COLOR } from "../../pages/_app";
 
+const StyledSection = styled.section`
+  color: ${COLOR.light};
+  display: flex;
+  flex-direction: column;
+`;
 /**
  * @typedef {import("@prismicio/client").Content.HistorySlice} HistorySlice
  * @typedef {import("@prismicio/react").SliceComponentProps<HistorySlice>} HistoryProps
  * @param { HistoryProps }
  */
 const History = ({ slice }) => (
-  <section>
+  <StyledSection>
     <span className="title">
-      {
-        slice.primary.title ?
-        <PrismicRichText field={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
+      {slice.primary.title && <PrismicRichText field={slice.primary.title} />}
     </span>
-    {
-      slice.primary.description ?
-      <PrismicRichText field={slice.primary.description}/>
-      : <p>start by editing this slice from inside Slice Machine!</p>
-    }
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
-)
+    <div>
+      
+    </div>
+  </StyledSection>
+);
 
-export default History
+export default History;
